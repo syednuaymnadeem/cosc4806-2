7efe140206bbf6a7d52ff11633153ff70a9c111c
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-05-31 00:54:18 +0000

    Created user.php

--- database.php
+++ database.php
@@ -7,7 +7,6 @@ function db_connect() {
         $dbh = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
         return $dbh;
     } catch (PDOException $e) {
-        // We should set a global variable here so we know the DB is down
     }
 }
 
