5ac7630375c5106cb97e0ae7294910f7559264e2
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-05-31 01:03:07 +0000

    added the user class and get users function and sql statements from db_connect to select and execution

---
+++ user.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('database.php');
+
+class User {
+
+    public function get_all_users() {
+        $db = db_connect();
+        $statement = $db->prepare("select * from users;");
+        $statement->execute();
+        $rows = $statement->fetch(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+
+}
